05c99c95e47a6637d3cdde21801d572c8c3ce818
Yiğit Turan <yigitturan@Yigit-MacBook-Air.local>
2026-05-16 17:22:03 +0300

    refactor: remove unused reputation leaderboard from demo page

--- web/src/app/demo/page.tsx
+++ web/src/app/demo/page.tsx
@@ -5,8 +5,6 @@ import { TopBar } from "@/components/layout/TopBar";
 import { StatusBar } from "@/components/layout/StatusBar";
 import { DemoPanel } from "@/components/DemoPanel";
 import { VerificationFeed } from "@/components/VerificationFeed";
-import { ReputationLeaderboard } from "@/components/ReputationLeaderboard";
-
 export default function DemoPage() {
   return (
     <Providers>
@@ -14,16 +12,12 @@ export default function DemoPage() {
         <TopBar />
 
         <main className="flex-1 grid grid-cols-1 lg:grid-cols-12 gap-px bg-border-subtle overflow-hidden">
-          <div className="lg:col-span-5 bg-bg-base overflow-y-auto p-2 space-y-2">
+          <div className="lg:col-span-7 bg-bg-base overflow-y-auto p-2 space-y-2">
             <DemoPanel />
           </div>
 
-          <div className="lg:col-span-4 bg-bg-base overflow-y-auto p-2">
+          <div className="lg:col-span-5 bg-bg-base overflow-y-auto p-2">
             <VerificationFeed />
-          </div>
-
-          <div className="lg:col-span-3 bg-bg-base overflow-y-auto p-2">
-            <ReputationLeaderboard />
           </div>
         </main>
 
